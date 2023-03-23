b43ebab59b8a1c65d5078039fbe3ec83c7ea4d25
USER <USER@erpAS-note>
2023-03-23 11:32:59 +0900

    update Test

--- mkpenc-web/src/main/java/com/mkpenc/mark/mimic/web/MarkvMimicContentsController.java
+++ mkpenc-web/src/main/java/com/mkpenc/mark/mimic/web/MarkvMimicContentsController.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+    
 @Controller
 @RequestMapping("/markv/mimic/")
 public class MarkvMimicContentsController {
